778e844c9e2c946219bb0e6bc4ebc479ce630f20
Charles Nicoletti <cnicoletti@gmail.com>
2018-08-25 18:38:44 -0700

    Added JPA entities: - Ingredients - Notes - Recipe - UnitOfMeasure

---
+++ src/main/java/me/cheznic/learning/recipe/model/Notes.java
@@ -0,0 +1,44 @@
+package me.cheznic.learning.recipe.model;
+
+import javax.persistence.*;
+
+/**
+ * Created by Charles Nicoletti on 8/25/18
+ */
+@Entity
+public class Notes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Recipe recipe;
+
+    @Lob
+    private String recipeNotes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public String getRecipeNotes() {
+        return recipeNotes;
+    }
+
+    public void setRecipeNotes(String recipeNotes) {
+        this.recipeNotes = recipeNotes;
+    }
+}
---
+++ src/main/java/me/cheznic/learning/recipe/model/UnitOfMeasure.java
@@ -0,0 +1,34 @@
+package me.cheznic.learning.recipe.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * Created by Charles Nicoletti on 8/25/18
+ */
+@Entity
+public class UnitOfMeasure {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
