689324f2125389cd547332e5af9f1d84346c1f96
Charles Nicoletti <cnicoletti@gmail.com>
2018-08-25 17:45:44 -0700

    add all

---
+++ src/main/java/me/cheznic/learning/recipe/RecipeApplication.java
@@ -0,0 +1,12 @@
+package me.cheznic.learning.recipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeApplication.class, args);
+    }
+}
---
+++ src/main/java/me/cheznic/learning/recipe/controllers/IndexController.java
@@ -0,0 +1,17 @@
+package me.cheznic.learning.recipe.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Charles Nicoletti on 8/24/18
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "/index", "index.html"})
+    public String getIndexPage () {
+
+        return "index";
+    }
+}
