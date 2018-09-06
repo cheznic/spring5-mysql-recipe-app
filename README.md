9cfc67346e04b6fdf209fd40575454438f4a671b
Charles Nicoletti <cnicoletti@gmail.com>
2018-09-05 20:49:11 -0700

    Integrated Codecov.io service for code coverage reporting.

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-# spring5-recipe-app
+# Spring Boot Recipe Application
+
+[![CircleCI](https://circleci.com/gh/springframeworkguru/spring5-recipe-app.svg?style=svg)](https://circleci.com/gh/springframeworkguru/spring5-recipe-app)
+
+[![codecov](https://codecov.io/gh/springframeworkguru/spring5-mysql-recipe-app/branch/master/graph/badge.svg)](https://codecov.io/gh/springframeworkguru/spring5-mysql-recipe-app)
 
 Spring5/JPA application to refresh my knowledge on JPA
+
+Technologies used:
+- Spring 5
+- Java JPA
--- src/main/java/me/cheznic/learning/recipe/controllers/RecipeController.java
+++ src/main/java/me/cheznic/learning/recipe/controllers/RecipeController.java
@@ -8,8 +8,11 @@ import me.cheznic.learning.recipe.services.RecipeService;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
 
 /**
  * Created by Charles Nicoletti on 9/1/18
@@ -17,6 +20,8 @@ import org.springframework.web.servlet.ModelAndView;
 @Slf4j
 @Controller
 public class RecipeController {
+
+    private static final String RECIPE_RECIPEFORM_URL = "recipe/recipeform";
 
     private final RecipeService recipeService;
 
@@ -41,7 +46,7 @@ public class RecipeController {
     @GetMapping("recipe/new")
     public String newRecipe(Model model){
         model.addAttribute("recipe", new RecipeCommand());
-        return "recipe/recipeform";
+        return RECIPE_RECIPEFORM_URL;
     }
 
     @GetMapping("recipe/{id}/update")
@@ -54,11 +59,17 @@ public class RecipeController {
         }
 
         model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
-        return "recipe/recipeform";
+        return RECIPE_RECIPEFORM_URL;
     }
 
     @PostMapping("recipe")
-    public String saveOrUpdate(@ModelAttribute RecipeCommand command){
+    public String saveOrUpdate(@Valid @ModelAttribute("recipe") RecipeCommand command, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            bindingResult.getAllErrors().forEach(objectError -> {
+                log.debug(objectError.toString());
+            });
+            return RECIPE_RECIPEFORM_URL;
+        }
         RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
 
         return "redirect:/recipe/" + savedCommand.getId() + "/show";
